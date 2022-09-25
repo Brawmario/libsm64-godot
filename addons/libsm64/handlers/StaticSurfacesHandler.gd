9d871353eb3e563e3db91d36329a472a01a5daa5
Renato Rotenberg <renato.rotenberg@gmail.com>
2022-09-25 18:19:34 -0300

    Convert project to Godot 4

--- addons/libsm64/handlers/StaticSurfacesHandler.gd
+++ addons/libsm64/handlers/StaticSurfacesHandler.gd
@@ -1,14 +1,14 @@
 extends Node
 
-export var sm64_handler: Resource
-export var static_surfaces_group := "libsm64_static_surfaces"
+@export var sm64_handler: Resource
+@export var static_surfaces_group := "libsm64_static_surfaces"
 
 
 func load_static_sufaces() -> void:
-	var faces := PoolVector3Array()
+	var faces := PackedVector3Array()
 
 	for mesh_instance in get_tree().get_nodes_in_group(static_surfaces_group):
-		var mesh_faces: PoolVector3Array = mesh_instance.get_mesh().get_faces()
+		var mesh_faces: PackedVector3Array = mesh_instance.get_mesh().get_faces()
 		var offset: Vector3 = mesh_instance.global_transform.origin
 		for i in range(mesh_faces.size()):
 			mesh_faces[i] += offset
