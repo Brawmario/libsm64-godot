ddd2a3eac426866d2169391f46899386d1239f1e
Renato Rotenberg <renato.rotenberg@gmail.com>
2023-10-05 23:23:04 -0300

    Avoid unecessary raw allocation

--- extension/src/sm64_global.cpp
+++ extension/src/sm64_global.cpp
@@ -50,14 +50,15 @@ void SM64Global::init()
 
     ERR_FAIL_COND_MSG(rom.is_empty(), "[libsm64-godot] Failed to read ROM file.");
 
-    constexpr int64_t mario_texture_size = 4 * SM64_TEXTURE_WIDTH * SM64_TEXTURE_HEIGHT;
-    uint8_t *mario_texture_raw = (uint8_t *) malloc(mario_texture_size);
+    godot::PackedByteArray mario_texture_bytes;
+    constexpr int64_t mario_texture_bytes_size = 4 * SM64_TEXTURE_WIDTH * SM64_TEXTURE_HEIGHT;
+    mario_texture_bytes.resize(mario_texture_bytes_size);
 
 // #ifdef DEBUG_ENABLED
 //     sm64_register_debug_print_function(SM64DebugPrintFunction);
 // #endif // DEBUG_ENABLED
 
-    sm64_global_init(rom.ptr(), mario_texture_raw);
+    sm64_global_init(rom.ptr(), mario_texture_bytes.ptrw());
 
     // sm64_audio_init leaks audio everytime it's called, make sure it's only called once
     static bool s_is_audio_init = false;
@@ -75,16 +76,10 @@ void SM64Global::init()
     #endif // HAVE_WASAPI
     sm64_native_audio_start();
 
-    godot::PackedByteArray mario_texture_packed;
-    mario_texture_packed.resize(mario_texture_size);
-    memcpy(mario_texture_packed.ptrw(), mario_texture_raw, mario_texture_size);
-
-    m_mario_image = godot::Image::create_from_data(SM64_TEXTURE_WIDTH, SM64_TEXTURE_HEIGHT, false, godot::Image::FORMAT_RGBA8, mario_texture_packed);
+    m_mario_image = godot::Image::create_from_data(SM64_TEXTURE_WIDTH, SM64_TEXTURE_HEIGHT, false, godot::Image::FORMAT_RGBA8, mario_texture_bytes);
     m_mario_image_texture = godot::ImageTexture::create_from_image(m_mario_image);
 
     m_init = true;
-
-    ::free(mario_texture_raw);
 }
 
 void SM64Global::terminate()
