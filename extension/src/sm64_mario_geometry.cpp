1b8095047dc9b99383156516ded6f61200bf0b6b
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-01-01 12:05:18 -0300

    Adjust SM64MarioGeometry interpolation

--- extension/src/sm64_mario_geometry.cpp
+++ extension/src/sm64_mario_geometry.cpp
@@ -1,16 +1,9 @@
 #include <sm64_mario_geometry.hpp>
 
-#include <algorithm>
-
-#define LERP(last, current, amount) (last + (current - last) * amount)
+#include <godot_cpp/core/math.hpp>
 
 SM64MarioGeometry::SM64MarioGeometry()
 {
-    position.fill(0.0f);
-    normal.fill(0.0f);
-    color.fill(0.0f);
-    uv.fill(0.0f);
-
     geometry.position = position.data();
     geometry.normal = normal.data();
     geometry.color = color.data();
@@ -48,13 +41,13 @@ SM64MarioGeometry &SM64MarioGeometry::operator=(const SM64MarioGeometry &other)
 
 void SM64MarioGeometry::lerp(const SM64MarioGeometry &last, const SM64MarioGeometry &current, float amount)
 {
-    geometry.numTrianglesUsed = current.geometry.numTrianglesUsed;
+    geometry.numTrianglesUsed = godot::MIN(last.geometry.numTrianglesUsed, current.geometry.numTrianglesUsed);
 
     for (int i = 0; i < 9 * geometry.numTrianglesUsed; i++)
-        position[i] = LERP(last.position[i], current.position[i], amount);
+        position[i] = godot::Math::lerp(last.position[i], current.position[i], amount);
 
     for (int i = 0; i < 9 * geometry.numTrianglesUsed; i++)
-        normal[i] = LERP(last.normal[i], current.normal[i], amount);
+        normal[i] = godot::Math::lerp(last.normal[i], current.normal[i], amount);
 
     color = current.color;
     uv = current.uv;
