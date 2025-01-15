2caf0be6626f7d599c519a36918a24f87c0f2ec8
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-01-14 23:15:45 -0300

    Finally clean up all the old classes

--- extension/src/register_types.cpp
+++ extension/src/register_types.cpp
@@ -7,14 +7,6 @@
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/godot.hpp>
 
-#include <sm64_global.hpp>
-#include <sm64_surfaces.hpp>
-//#include <sm64_audio.hpp>
-#include <sm64_mario_internal.hpp>
-#include <sm64_input.hpp>
-#include <sm64_surface_properties.hpp>
-#include <sm64_audio_stream_player.hpp>
-
 #include <libsm64.hpp>
 #include <libsm64_surface_properties.hpp>
 #include <libsm64_mario_inputs.hpp>
@@ -26,23 +18,12 @@
 
 using namespace godot;
 
-static SM64Global *s_sm64_global;
-static SM64Surfaces *s_sm64_surfaces;
-//static SM64Audio *s_sm64_audio;
 static LibSM64 *s_libsm64;
 
 void initialize_libsm64gd_module(ModuleInitializationLevel p_level) {
     if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
         return;
     }
-
-    ClassDB::register_class<SM64Global>();
-    ClassDB::register_class<SM64Surfaces>();
-    //ClassDB::register_class<SM64Audio>();
-    ClassDB::register_class<SM64MarioInternal>();
-    ClassDB::register_class<SM64Input>();
-    ClassDB::register_class<SM64SurfaceProperties>();
-    ClassDB::register_class<SM64AudioStreamPlayer>();
 
     ClassDB::register_class<LibSM64>();
     ClassDB::register_class<LibSM64SurfaceProperties>();
@@ -53,15 +34,6 @@ void initialize_libsm64gd_module(ModuleInitializationLevel p_level) {
     ClassDB::register_class<LibSM64MarioInternal>();
     ClassDB::register_class<LibSM64AudioStreamPlayer>();
 
-    s_sm64_global = memnew(SM64Global);
-    Engine::get_singleton()->register_singleton("SM64Global", SM64Global::get_singleton());
-
-    s_sm64_surfaces = memnew(SM64Surfaces);
-    Engine::get_singleton()->register_singleton("SM64Surfaces", SM64Surfaces::get_singleton());
-
-    //s_sm64_audio = memnew(SM64Audio);
-    //Engine::get_singleton()->register_singleton("SM64Audio", SM64Audio::get_singleton());
-
     s_libsm64 = memnew(LibSM64);
     Engine::get_singleton()->register_singleton("LibSM64", LibSM64::get_singleton());
 }
@@ -70,15 +42,6 @@ void uninitialize_libsm64gd_module(ModuleInitializationLevel p_level) {
     if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
         return;
     }
-
-    Engine::get_singleton()->unregister_singleton("SM64Global");
-    memdelete(s_sm64_global);
-
-    Engine::get_singleton()->unregister_singleton("SM64Surfaces");
-    memdelete(s_sm64_surfaces);
-
-    //Engine::get_singleton()->unregister_singleton("SM64Audio");
-    //memdelete(s_sm64_audio);
 
     Engine::get_singleton()->unregister_singleton("LibSM64");
     memdelete(s_libsm64);
