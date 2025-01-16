d85fb85b6405a35ed4ed6dbe0e8155a78862dbb9
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-01-15 22:38:01 -0300

    Documentation work

--- addons/libsm64-godot/components/libsm64_surface_properties_component.gd
+++ addons/libsm64-godot/components/libsm64_surface_properties_component.gd
@@ -1,6 +1,7 @@
 class_name LibSM64SurfacePropertiesComponent
 extends Node
 
-## Component that holds a ``LibSM64SurfaceProperties`` resource to be read by the SM64 Surface Handler nodes.
+## Component that holds a [LibSM64SurfaceProperties] resource to be read by the LibSM64 Surface Handler nodes.
 
+## [LibSM64SurfaceProperties] resource to be read by the LibSM64 Surface Handler nodes.
 @export var surface_properties: LibSM64SurfaceProperties
