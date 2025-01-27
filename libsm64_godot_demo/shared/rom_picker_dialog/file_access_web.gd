d6a3c0b80e23e2285e07d04202bc94f7547c048d
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-01-27 13:32:16 -0300

    Incorporate FileAccessWeb into demo project and insert its license notice

--- libsm64_godot_demo/shared/rom_picker_dialog/file_access_web.gd
+++ libsm64_godot_demo/shared/rom_picker_dialog/file_access_web.gd
@@ -1,6 +1,33 @@
 class_name FileAccessWeb
 extends RefCounted
 
+# From https://github.com/Scrawach/godot-file-access-web
+# Downloaded on 2025-01-26
+#
+# License:
+#
+# MIT License
+#
+# Copyright (c) 2023 Scrawach
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 signal load_started(file_name: String)
 signal loaded(file_name: String, file_type: String, base64_data: String)
 signal progress(current_bytes: int, total_bytes: int)
