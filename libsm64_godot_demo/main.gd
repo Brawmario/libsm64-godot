a599318beb7a9bd23b723dee74c0e0ac99b00de9
Renato Rotenberg <renato.rotenberg@gmail.com>
2025-01-26 22:23:53 -0300

    Add FileAccessWeb

--- libsm64_godot_demo/main.gd
+++ libsm64_godot_demo/main.gd
@@ -1,12 +1,12 @@
 extends Control
 
 
-@onready var rom_picker_dialog := $RomPickerDialog as FileDialog
+var _file_access_web: FileAccessWeb
 
 
 func _ready() -> void:
 	if LibSM64Global.rom.is_empty():
-		%RomPickerDialog.popup_centered_ratio()
+		_ask_for_rom_file()
 
 
 func return_to_menu() -> void:
@@ -17,6 +17,23 @@ func return_to_menu() -> void:
 func load_level(scene: PackedScene) -> void:
 	%LevelContainer.add_child(scene.instantiate())
 	hide()
+
+
+func _ask_for_rom_file() -> void:
+	if OS.get_name() == "Web":
+		_file_access_web = FileAccessWeb.new()
+		_file_access_web.loaded.connect(_on_file_access_web_loaded)
+		_file_access_web.open(".z64")
+	else:
+		%RomPickerDialog.popup_centered_ratio()
+
+
+func _on_file_access_web_loaded(_file_name: String, _type: String, base64_data: String) -> void:
+	LibSM64Global.rom = Marshalls.base64_to_raw(base64_data)
+
+
+func _on_pick_rom_button_pressed() -> void:
+	_ask_for_rom_file()
 
 
 func _on_initial_scenario_button_pressed() -> void:
