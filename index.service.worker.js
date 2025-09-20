7ae38841de00fc0d27fa8f658dd2075d4aa43d77
Brawmario <38757897+Brawmario@users.noreply.github.com>
2025-09-20 23:45:14 +0000

    Deploying to gh-pages from @ Brawmario/libsm64-godot@69a3e9616cd32ff70cc041ab51cb334935eec31a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1742344568|3311893';
+const CACHE_VERSION = '1758411871|3328986';
 /** @type {string} */
 const CACHE_PREFIX = 'LibSM64 Godot De-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
